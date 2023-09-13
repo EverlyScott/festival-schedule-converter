576af1356e5973301425693ed6d6b393ec0f5d86
Everly Scott <everly@everly.page>
2023-09-12 23:47:21 -0500

    Add loading screen & add button aria names

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import { CircularProgress } from "@mui/material";
+import { NextPage } from "next";
+
+const Loading: NextPage = () => {
+  return <CircularProgress />;
+};
+
+export default Loading;
--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -10,6 +10,7 @@ import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 // import theme toggle context
 import { ThemeToggleContext } from "./context";
+import { DarkMode, LightMode } from "@mui/icons-material";
 
 export function ThemeToggle() {
   // check theme is dark or light
@@ -19,8 +20,13 @@ export function ThemeToggle() {
   const themeToggle = useContext(ThemeToggleContext);
 
   return (
-    <IconButton sx={{ ml: 1 }} onClick={themeToggle.toggleColorMode} color="inherit">
-      {theme.palette.mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+    <IconButton
+      aria-label={theme.palette.mode === "dark" ? "Enable Light Mode" : "Enable Dark Mode"}
+      sx={{ ml: 1 }}
+      onClick={themeToggle.toggleColorMode}
+      color="inherit"
+    >
+      {theme.palette.mode === "dark" ? <LightMode /> : <DarkMode />}
     </IconButton>
   );
 }
